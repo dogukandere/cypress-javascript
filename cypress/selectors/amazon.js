c9aa72bd75b397107e1056dd7b748385f2102fc8
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-12-15 13:09:45 +0300

    changed

--- cypress/selectors/amazon.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    popupAccept: "#sp-cc-accept",
-    searchBox: "#twotabsearchtextbox",
-    searchButton: "#nav-search-submit-button",
-    productImage: "img[alt='Sponsorlu Reklam - Apple iPhone 15 (256 GB) - Sarı']",
-    
-}
---
+++ cypress/util/amazon.js
@@ -0,0 +1,7 @@
+module.exports = {
+    popupAccept: "#sp-cc-accept",
+    searchBox: "#twotabsearchtextbox",
+    searchButton: "#nav-search-submit-button",
+    productImage: "img[alt='Sponsorlu Reklam - Apple iPhone 15 (256 GB) - Sarı']",
+    example: "bla"
+}
