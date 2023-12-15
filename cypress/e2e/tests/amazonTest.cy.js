85a60d817dceea561cafb9d96ee9c92b27bfc25e
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-12-15 12:41:54 +0300

    changed

--- cypress/e2e/tests/amazonTest.cy.js
+++ cypress/e2e/tests/amazonTest.cy.js
@@ -1,19 +1,20 @@
 /// <reference types="cypress" />
 
+import amazon from "../../selectors/amazon"
+
 describe('amazonTest', () => {
 
     beforeEach(() => {
       cy.visit("https://www.amazon.com.tr/")
     })
 
-    it('search product and click product', () => {
-      cy.get("#sp-cc-accept").click()
-      cy.get("#twotabsearchtextbox").type('s23')
-      cy.get("#nav-search-submit-button").click()
+    it('search iphone 15', () => {
+      cy.get(amazon.popupAccept).click()
+      cy.get(amazon.searchBox).type('iphone 15')
+      cy.get(amazon.searchButton).click()
 
-      cy.get("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_2'] span[class='a-size-base-plus a-color-base a-text-normal']").click()
-      cy.url().should("include", "Samsung")
-      cy.get("body > div:nth-child(5) > div:nth-child(71) > div:nth-child(9) > div:nth-child(7) > div:nth-child(4) > div:nth-child(4) > div:nth-child(2) > span:nth-child(3) > a:nth-child(1) > span:nth-child(1)").click()
+      cy.get(amazon.productImage).click()
+      cy.url().should("include", "Apple")
       cy.wait(4000)
     })
   })
